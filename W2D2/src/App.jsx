2e1abdfb40dd62b776c506175d7d2ba5ac41caf7
WebikaS <vik24031995@gmail.com>
2022-05-29 12:20:14 +0530

    p

--- W2D2/src/App.jsx
+++ W2D2/src/App.jsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useRef, useState } from 'react';
+import {  useState } from 'react';
 import './App.css';
 import { Timer } from './components/Timer';
 
@@ -7,19 +7,7 @@ function App() {
   const [show, setShow] = useState(true);
 
 
-  // const ref = useRef(null);
-
-  // const [counter, setCounter] = useState(0);
-
-  // useEffect(() => {
-  //   startTimer();
-  // }, []);
-  
-  // const startTimer = () => {
-  //       ref.current = setInterval(() => {
-  //       setCounter((prev) => prev + 1)
-  //     },1000)
-  // }
+ 
 
   return (
     <div className="App">
@@ -28,15 +16,7 @@ function App() {
         setShow(show ? false : true);
         setShow(!show)
       }}>{show? "stop":"start" }</button>
-      {/* <h1>conter: {counter}</h1>
-      <button onClick={() => {
-        clearInterval(ref.current);
-      }}>stop</button>
-      <button onClick={startTimer}>start</button>
-      <button onClick={() => {
-        clearInterval(ref.current);
-        setCounter(0)
-      }}>reset</button> */}
+      
     </div>
   );
 }
